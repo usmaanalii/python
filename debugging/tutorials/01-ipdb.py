782bfdc6f84afb6b7549d564820935a6afc61739
usyyy <usmaanalii@hotmail.co.uk>
2017-08-10 14:24:44 +0100

    Completed an ipdb tutorial
    - Can be found at
    http://www.christianlong.com/blog/ipdb-the-ipython-debugger.html

---
+++ debugging/tutorials/01-ipdb.py
@@ -0,0 +1,15 @@
+# http://www.christianlong.com/blog/ipdb-the-ipython-debugger.html
+def main():
+    """
+    Main Function
+    """
+
+    x = 34
+    y = 3
+    z = y / x
+
+    pritn(x, y, z)
+
+
+if __name__ == '__main__':
+    main()
